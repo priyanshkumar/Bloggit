7a48d39270fd8a635fbdcf19635aa764d2db58e3
Santosh Kumar <kasantosh@yahoo.com>
2019-03-26 18:54:41 -0400

    Changed the apilist.js file

--- assets/javascript/apilist.js
+++ assets/javascript/apilist.js
@@ -1,27 +1,29 @@
 $(document).ready(function() {
   var searchTerm;
-  var retrieve;
 
   $("#searchApi").on("click", function(e) {
     e.preventDefault();
+    searchTerm = $("#searchTerm").val();
     var queryUrl =
       "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
       searchTerm +
       "&api-key=ACd4bRkDMq5GIjGoCxZpHjihygGZYp2g";
-    searchTerm = $("#searchTerm").val();
-    retrieve = parseInt($("#recordstoRetrieve").val());
-    console.log(retrieve);
-    console.log(typeof retrieve);
-
+    console.log(searchTerm);
     $.ajax({
       url: queryUrl,
       method: "GET"
     }).then(function(response) {
-      console.log(response.response.docs);
+      // console.log(response.response.docs);
       // console.log(response.response.docs[0].headline.main);
       // console.log(response.response.docs[0].byline.original);
       // console.log(response.response.docs[0].snippet);
       // console.log(response.response.docs[0].web_url);
+
+      var headline = response.response.docs[0].headline.main;
+      var author = response.response.docs[0].byline.original;
+      var snippet = response.response.docs[0].snippet;
+      var newsAPIurl = response.response.docs[0].web_url;
+      console.log(headline, author, snippet, newsAPIurl);
     });
   });
 });
